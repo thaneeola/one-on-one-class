3d4ef6dbaa6a92495f1559d0a7b8a97df539bea3
thaneeola <imranthanee@gmail.com>
2024-09-16 15:13:50 +0100

    modified loops

--- loops.js
+++ loops.js
@@ -81,31 +81,69 @@ for (let i = 0; i <= 10; i++) {
  10   100   1000 */
 
 for (let i = 0; i <= 10; i++) {
-  console.log(`${i}     ${i**2}     ${i**3}`);
+  console.log(`${i}     ${i ** 2}     ${i ** 3}`);
 }
-
-
 
 /* 7. Use for loop to iterate from 0 to 100 and print only even numbers*/
 
-for (let i = 1; i<=100;i++){
-  if(i%2==0){
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 0) {
     console.log(i);
   }
 }
 
 /* 8 Use for loop to iterate from 0 to 100 and print only odd numbers */
 for (let i = 1; i <= 100; i++) {
-  if(i%2!==0){
-    console.log(i)
+  if (i % 2 !== 0) {
+    console.log(i);
   }
 }
 
 /* 9. Use for loop to iterate from 0 to 100 and print only prime numbers */
 for (let i = 2; i <= 100; i++) {
-if(i % 1 == 0 && i % i ==0){
-  console.log(i)
+  if (i % 1 == 0 && i % i == 0) {
+    console.log(i);
+  }
 }
 
-}
+/* 10.  Use for loop to iterate from 0 to 100 and print the sum of all numbers.
 
+The sum of all numbers from 0 to 100 is 5050. */
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+  sum += i;
+}
+console.log(`The sum of all numbers from 0 to 100 is ${sum}`);
+
+/* 11. Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+
+The sum of all evens from 0 to 100 is 2550. And the sum of all odds from 0 to 100 is 2500.*/
+
+let sumEvens = 0;
+let sumOdds = 0;
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 == 0) {
+    sumEvens += i;
+  } else {
+    sumOdds += i;
+  }
+}
+console.log(
+  `The sum of all evens from 0 to 100 is ${sumEvens}. And the sum of all odds from 0 to 100 is ${sumOdds}`
+);
+
+/*12. Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds. Print sum of evens and sum of odds as array
+
+  [2550, 2500]*/
+let array = [];
+array.push(sumEvens);
+array.push(sumOdds);
+console.log(array);
+
+/* 13. Develop a small script which generate array of 5 random numbers */
+let array2 = [];
+for (let i = 0; i < 5; i++) {
+  let random = Math.floor(Math.random() * 100);
+  array2.push(random);
+}
+console.log(array2);
